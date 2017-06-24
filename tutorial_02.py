f6873a987e931494d03f6ae7b6d2bfef16cbfa32
khyox <jse.mnl@gmail.com>
2017-06-24 23:49:12 +0200

    Correct tutorial_02.py
    Changes to be committed:
    	modified:   tutorial_02.py

--- tutorial_02.py
+++ tutorial_02.py
@@ -15,6 +15,8 @@ y_ = tf.placeholder(tf.float32, [None, 10])
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y),
                                               reduction_indices=[1]))
 
+train_step = tf.train.GradientDescentOptimizer(0.05).minimize(cross_entropy)
+
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
@@ -23,9 +25,10 @@ tf.global_variables_initializer().run()
 
 for _ in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
 
-# [Jose]: With GPU, I get 0.098 here too
+# [Jose]: With GPU, I get 0.9002 here
